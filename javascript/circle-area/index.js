42da06a54cbba555bfa2ed3af65bc4a7160cee42
chris-to-fer <mail@christopherhaug.de>
2023-12-07 13:58:57 +0100

    kickoff circle area challenge

---
+++ javascript/circle-area/index.js
@@ -0,0 +1,27 @@
+## Calculate Circle Area and Circumference 🌶️🌶️🌶️
+
+### Steps
+
+1. Create a new JavaScript file in your preferred code editor (e.g., Visual
+   Studio Code).
+2. Initialize a variable to represent the radius of a circle. For example, you
+   can set radius to a specific value, like 5.
+3. Calculate the area of the circle using the formula
+   [Math.PI](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI) \*
+   [Math.pow](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow)(radius, 2)
+   and store the result in a variable called circleArea.
+4. Calculate the circumference of the circle using the formula 2 _
+   [Math.PI](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI)
+   _ radius and store the result in a variable called circumference.
+5. Display the results by logging them to the console. You can use console.log
+   to print the area and circumference. For example:
+
+   ```md
+   For a circle with a radius of 5 units:
+
+   -   The area is approximately 78.54 square units.
+   -   The circumference is approximately 31.42 units.
+   ```
+
+6. Run your JavaScript program to see the calculated area and circumference of
+   the circle with the specified radius.
