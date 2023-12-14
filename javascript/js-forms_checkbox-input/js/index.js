d7d8c8b42860397688f5c29974a6e7796762073f
chris-to-fer <mail@christopherhaug.de>
2023-12-14 11:59:48 +0100

    checkbox done

--- javascript/js-forms_checkbox-input/js/index.js
+++ javascript/js-forms_checkbox-input/js/index.js
@@ -12,13 +12,30 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+addEventListener("load", (e) => {
+  hideTosError();
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
 
+  if (event.target.elements.tos.checked === false) {
+    return;
+  }
+
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+});
+
+tosCheckbox.addEventListener("input", (e) => {
+  console.log(e.target.checked);
+  if (e.target.checked === false) {
+    showTosError();
+  } else if (e.target.checked === true) {
+    hideTosError();
+  }
 });
