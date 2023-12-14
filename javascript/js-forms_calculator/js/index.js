bfef568ed82d9fb5f7dbaf61c68e72eb83337781
chris-to-fer <mail@christopherhaug.de>
2023-12-14 13:02:55 +0100

    calculator done

--- javascript/js-forms_calculator/js/index.js
+++ javascript/js-forms_calculator/js/index.js
@@ -25,7 +25,24 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  const stuff = new FormData(event.target);
+  const data = Object.fromEntries(stuff);
+  const a = parseInt(data.numberA);
+  const b = parseInt(data.numberB);
 
+  if (data.operator.charAt(0) === "a") {
+    result = add(a, b);
+  }
+  if (data.operator.charAt(0) === "s") {
+    result = subtract(a, b);
+  }
+  if (data.operator.charAt(0) === "m") {
+    result = multiply(a, b);
+  }
+  if (data.operator.charAt(0) === "d") {
+    result = divide(a, b);
+  }
+  console.log(result);
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
