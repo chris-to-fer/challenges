add067e68654d374765a6360af1c1b9265b546d4
chris-to-fer <mail@christopherhaug.de>
2023-12-15 12:12:00 +0100

    done product teaser

--- javascript/js-createelement_product-teaser/js/index.js
+++ javascript/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,41 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const body = document.querySelector("body");
+
+const teaserBody = document.createElement("article");
+teaserBody.classList.add("product");
+body.append(teaserBody);
+teaserBody.innerHTML = `
+<div class="product__body">
+<div class="product__text-container">
+  <h2 class="product__name">${name}</h2>
+  <ul class="product__categories">
+    <li class="product__category">${category1}</li>
+    <li class="product__category">${category2}</li>
+    <li class="product__category">${category3}</li>
+  </ul>
+  <p class="product__description">
+  ${description}
+  </p>
+</div>
+<div class="product__image-container">
+  <img
+    class="product__image"
+    src=${imageSrc}
+    alt=""
+  />
+</div>
+</div>
+<footer class="product__footer">
+<span class="product__price">${price} </span>
+<button type="button" class="product__buy-button" data-js="buyButton2">Buy</button>
+</footer>
+
+`;
+const button2 = document.querySelector('[data-js="buyButton2"]');
+
+button2.addEventListener("click", () => {
+  console.log(`${name} costs ${price}`);
+});
