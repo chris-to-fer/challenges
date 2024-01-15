3877fd27cf0912cda0e1263e7eb99c5a07d94951
chris-to-fer <mail@christopherhaug.de>
2024-01-15 15:14:25 +0100

    done sum

--- javascript/react-props_sum/src/App.js
+++ javascript/react-props_sum/src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA="7" valueB="5" />;
 }
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {valueA} + {valueB} = {parseInt(valueA) + parseInt(valueB)}{" "}
+    </h1>
+  );
+}
+
+/*
+Write the `Sum` component inside of the `src/App.js`.
+  - It accepts two props called `valueA` and `valueB` (make sure to destructure them).
+  - It returns an HTML element and uses both props to dynamically render something like "1 + 2 = 3" (according to the props).
+- Inside of the return statement of your `App` component, replace the heading with your `Sum` component and pass it both props with values of your choice.
+*/
