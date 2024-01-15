f303853d361718455398dfe083292f77d975f855
chris-to-fer <mail@christopherhaug.de>
2024-01-15 16:38:04 +0100

    done button

--- javascript/react-props_button/src/App.js
+++ javascript/react-props_button/src/App.js
@@ -1,6 +1,24 @@
+//console.clear();
 import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    return () => console.log("OK");
+  }
+  return <Button color=" #FFA500" text="Press me" onClick={handleClick} />;
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: color }}
+        disabled={disabled}
+        onClick={onClick()}
+      >
+        {text}
+      </button>
+    </div>
+  );
 }
