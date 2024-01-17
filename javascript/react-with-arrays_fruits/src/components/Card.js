d297d5cef81cfa43c9f8c0fbb0d9471ba1347138
chris-to-fer <mail@christopherhaug.de>
2024-01-17 17:53:19 +0100

    second solution css

--- javascript/react-with-arrays_fruits/src/components/Card.js
+++ javascript/react-with-arrays_fruits/src/components/Card.js
@@ -2,8 +2,7 @@ import "./Card.css";
 
 export default function Card({ name, color }) {
   return (
-    <p className="card" style={{ backgroundColor: `${color}` }}>
-      {name}
-    </p>
+    // <p className="card" style={{ backgroundColor: `${color}` }}>
+    <p className={`card card--${color}`}>{name}</p>
   );
 }
