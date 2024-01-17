73858c621b23482ce03ea0921b8560f8b6f5cb89
chris-to-fer <mail@christopherhaug.de>
2024-01-17 17:33:11 +0100

    another css solution

--- javascript/react-with-arrays_users/src/components/Tag.js
+++ javascript/react-with-arrays_users/src/components/Tag.js
@@ -2,8 +2,6 @@ import "./Tag.css";
 
 export default function Tag({ tag }) {
   return (
-    <li className={tag === "admin" ? "tag" + " " + "tag--highlight" : "tag"}>
-      {tag}
-    </li>
+    <li className={tag === "admin" ? "tag tag--highlight" : "tag"}>{tag}</li>
   );
 }
