012f21724a7fccf59fb9c847485f70b898fdcdea
chris-to-fer <mail@christopherhaug.de>
2024-01-17 10:40:23 +0100

    done counter

--- javascript/react-state_counter/src/App.js
+++ javascript/react-state_counter/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
-
+  const [count, setCount] = useState(0);
   return (
     <div className="container">
       <h1>{count}</h1>
@@ -11,7 +11,8 @@ export default function App() {
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            setCount(count - 1);
+            console.log("😊");
           }}
         >
           -
@@ -19,7 +20,8 @@ export default function App() {
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            setCount(count + 1);
+            console.log("😙");
           }}
         >
           +
