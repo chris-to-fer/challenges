d8eb7668c0770e73491e46aaae56e2207b2e016b
chris-to-fer <mail@christopherhaug.de>
2024-02-09 12:55:06 +0100

    make swr global

--- javascript/backend-api-routes_products/pages/products/[id].js
+++ javascript/backend-api-routes_products/pages/products/[id].js
@@ -3,29 +3,14 @@ import useSWR from "swr";
 
 import React from "react";
 
-const fetcher = async (url) => {
-  const res = await fetch(url);
-
-  // If the status code is not in the range 200-299,
-  // we still try to parse and throw it.
-  if (!res.ok) {
-    const error = new Error("An error occurred while fetching the data.");
-    // Attach extra info to the error object.
-    error.info = await res.json();
-    error.status = res.status;
-    throw error;
-  }
-
-  return res.json();
-};
-
 export default function Details() {
   const router = useRouter();
   const id = router.query.id;
   console.log(id);
   const url = `/api/products/${id}`;
   console.log(url);
-  const { data, error, isLoading } = useSWR(url, fetcher);
+
+  const { data, error, isLoading } = useSWR(url);
   if (error) return <div>{error.message}</div>;
   if (isLoading) return <div>loading...</div>;
   if (!data) {
--- javascript/backend-api-routes_products/pages/products/index.js
+++ javascript/backend-api-routes_products/pages/products/index.js
@@ -1,25 +1,24 @@
 import useSWR from "swr";
 import { SWRConfig } from "swr";
 
-const url = "/api/products";
-const fetcher = async (url) => {
-  const res = await fetch(url);
+// const fetcher = async (url) => {
+//   const res = await fetch(url);
 
-  // If the status code is not in the range 200-299,
-  // we still try to parse and throw it.
-  if (!res.ok) {
-    const error = new Error("An error occurred while fetching the data.");
-    // Attach extra info to the error object.
-    error.info = await res.json();
-    error.status = res.status;
-    throw error;
-  }
+//   // If the status code is not in the range 200-299,
+//   // we still try to parse and throw it.
+//   if (!res.ok) {
+//     const error = new Error("An error occurred while fetching the data.");
+//     // Attach extra info to the error object.
+//     error.info = await res.json();
+//     error.status = res.status;
+//     throw error;
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 export default function ProductsList() {
-  const { data, error, isLoading } = useSWR(`api/products/`, fetcher);
+  const { data, error, isLoading } = useSWR(`/api/products/`);
 
   if (error) return <div>{error.message}</div>;
   if (isLoading) return <div>loading...</div>;
